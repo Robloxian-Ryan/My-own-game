35047679f79c27f4881b110bd565fda535285e6d
Robloxian-Ryan <67097715+Robloxian-Ryan@users.noreply.github.com>
2022-04-03 12:10:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,29 @@
 var bg,bgImg;
 var player, idleA, jumpA, runA, deadA;
 var ground, groundImg;
-var tree1, tree2, tree3;
-var gameState;
+var tree1, tree2, tree3, mushroom1, mushroom2, rasengan, obstacle, obstaclesGroup;
+var jumpState, singleJumpTime;
 
 function preload(){
   bgImg = loadImage("assets/BG.png")
   
-  idleA = loadAnimation("assets/Idle (1).png", "assets/Idle (2).png", "assets/Idle (3).png", "assets/Idle (4).png", "assets/Idle (5).png", "assets/Idle (6).png", "assets/Idle (7).png", "assets/Idle (8).png", "assets/Idle (9).png", "assets/Idle (10).png", "assets/Idle (11).png", "assets/Idle (12).png", "assets/Idle (13).png", "assets/Idle (14).png", "assets/Idle (15).png")
-  jumpA = loadAnimation("assets/Jump (1).png", "assets/Jump (2).png", "assets/Jump (3).png", "assets/Jump (4).png", "assets/Jump (5).png", "assets/Jump (6).png", "assets/Jump (7).png", "assets/Jump (8).png", "assets/Jump (9).png", "assets/Jump (10).png", "assets/Jump (11).png", "assets/Jump (12).png", "assets/Jump (13).png", "assets/Jump (14).png", "assets/Jump (15).png")
-  runA = loadAnimation("assets/Run (1).png", "assets/Run (2).png", "assets/Run (3).png", "assets/Run (4).png", "assets/Run (5).png", "assets/Run (6).png", "assets/Run (7).png", "assets/Run (8).png", "assets/Run (9).png", "assets/Run (10).png", "assets/Run (11).png", "assets/Run (12).png", "assets/Run (13).png", "assets/Run (14).png", "assets/Run (15).png")
-  deadA = loadAnimation("assets/Dead (1).png", "assets/Dead (2).png", "assets/Dead (3).png", "assets/Dead (4).png", "assets/Dead (5).png", "assets/Dead (6).png", "assets/Dead (7).png", "assets/Dead (8).png", "assets/Dead (9).png", "assets/Dead (10).png", "assets/Dead (11).png", "assets/Dead (12).png", "assets/Dead (13).png", "assets/Dead (14).png", "assets/Dead (15).png")
+  jumpA = loadImage("assets/Jump (7).png");
+  runA = loadAnimation("assets/Run (1).png", "assets/Run (3).png", "assets/Run (5).png", "assets/Run (6).png", "assets/Run (8).png", "assets/Run (9).png", "assets/Run (12).png", "assets/Run (13).png", "assets/Run (15).png");
+  deadA = loadImage("assets/Dead (15).png");
 
   groundImg = loadImage("assets/groundImg.png");
 
   tree1 = loadImage("assets/Tree_1.png");
   tree2 = loadImage("assets/Tree_2.png");
   tree3 = loadImage("assets/Tree_3.png");
+
+  mushroom1 = loadImage("assets/Mushroom_1.png");
+  mushroom2 = loadImage("assets/Mushroom_2.png");
+
+  rasengan = loadImage("assets/magik.png");
+  
+  jumpState = 0;
+  singleJumpTime = 0;
 }
 
 function setup() { 
@@ -27,9 +34,8 @@ function setup() {
   bg.scale = 1.5;
   
   player = createSprite(windowWidth-1150, windowHeight-300, 50, 50);
-  player.addAnimation("idle", idleA);
-  player.addAnimation("jump", jumpA);
   player.addAnimation("run", runA);
+  player.addAnimation("jump", jumpA);
   player.addAnimation("dead", deadA);
   player.scale = 0.4;
   player.debug = true;
@@ -43,22 +49,64 @@ function setup() {
 function draw() {
   background(0);
   player.collide(ground);
+  console.log(singleJumpTime);
   
   if(keyCode === 32){
     player.changeAnimation("jump", jumpA);
-    player.velocityY = -6;
+    player.y = 500;
+    singleJumpTime += 1;
   }
-  else if(keyCode === 39){
-    player.changeAnimation("run", runA);
+  else if(player.y >= 842.6){
+    player.changeAnimation("jump", jumpA);
   }
   else{
-    player.changeAnimation("idle", idleA);
+    player.y = 842.5;
+    player.changeAnimation("run", runA);
   }
   
-  if(ground.x < -10){
+  if(ground.x < 200){
     ground.x = ground.width/2;
   }
+
+  if(singleJumpTime > 25){
+    player.y = 842;
+    singleJumpTime = 0;
+    keyCode = 39;
+  }
+
+  player.velocityY += 5;
   
-  player.velocityY += 6;
+  spawnObstacles1();
   drawSprites();
+}
+
+function spawnObstacles1() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,height-95,20,30);
+    //obstacle.setCollider('circle',0,0,45);
+    obstacle.debug = true;
+  
+    obstacle.velocityX = ground.velocityX;
+    
+    var rand = Math.round(random(1, 3));
+
+    switch(rand) {
+      case 1:
+        obstacle.addImage(tree1);
+        break;
+      case 2:
+        obstacle.addImage(tree2);
+        break;
+      case 3:
+        obstacle.addImage(tree3);
+        break;
+    }
+    
+    obstacle.scale = 0.3;
+    obstacle.lifetime = 300;
+    obstacle.depth = player.depth;
+    player.depth +=1;
+    
+    obstaclesGroup.add(obstacle);
+  }
 }
